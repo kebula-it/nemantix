5d6418368d124b78dac0e68aee87cce059a4fe68
luca-anzalone <luca.anzalone@kebula.it>
2026-05-25 16:35:21 +0200

    event payload sanitization before DB saving

--- src/nemantix/hub/base.py
+++ src/nemantix/hub/base.py
@@ -1,6 +1,6 @@
 import datetime
-
-from typing import TYPE_CHECKING
+import json
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from nemantix.common.connectors import DBConnector
@@ -27,12 +27,36 @@ class Storable:
         # Convert float timestamp to datetime
         timestamp = kwargs.pop("timestamp")
         dt_timestamp = datetime.datetime.fromtimestamp(timestamp)
-
+        
         try:
+            payload = self._sanitize_payload(payload=kwargs.pop("payload", {}))
+            
             with self.connector.get_session() as session:
-                db_log = EventLogModel(timestamp=dt_timestamp, **kwargs)
+                db_log = EventLogModel(timestamp=dt_timestamp, 
+                                       payload=payload, **kwargs)
                 session.add(db_log)
                 session.commit()
 
         except Exception as e:
             print(f"Failed to write log to DB: {e}")
+
+    @staticmethod
+    def _sanitize_payload(payload: dict[str, Any]) -> dict[str, Any]:
+        """
+        Checks all fields in a dictionary. If a field is JSON-serializable,
+        it keeps it. If not, it converts it to a string representation to prevent errors.
+        """
+        if not isinstance(payload, dict):
+            return {"raw_data": str(payload)}
+
+        safe_payload = {}
+        for key, value in payload.items():
+            try:
+                json.dumps(value)
+                safe_payload[key] = value
+
+            except (TypeError, OverflowError):
+                safe_payload[key] = str(value)
+
+        return safe_payload
+
